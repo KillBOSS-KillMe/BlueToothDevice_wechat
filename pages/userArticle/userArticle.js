07a7793761503bce2959e371a607e6accafdfd00
luow <429175116@qq.com>
2019-08-24 18:00:13 +0800

    个人中心==>>我的帖子

--- pages/userArticle/userArticle.js
+++ pages/userArticle/userArticle.js
@@ -6,88 +6,125 @@ Page({
    * 页面的初始数据
    */
   data: {
-    luntanTxt: []
+    userInfo: {},
+    articleList: [],
+    articleListAll: [],
+    requestImgUrl: '',
+    originalImgUrl: ''
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {    
-    if (this.data.luntanTxt.length > 0) {
-      return ''
-    }
+    this.setData({
+      userInfo: app.globalData.userInfo,
+      requestImgUrl: app.globalData.requestImgUrl,
+      originalImgUrl: app.globalData.originalImgUrl
+    })
+    this.getArticleList()
+  },
+  getArticleList() {
     wx.request({
       url: `${app.globalData.requestUrl}/User/user_post`,
-      headers: {
-        'Content-Type': 'application/json'
-      },
       data: {
-        uid: '1'
+        uid: this.data.userInfo.id,
+        page: 0
       },
       method: "POST",
-      success: res => {
-        res = app.null2str(res.data)
-        if (res.code == '1') {
+      success: data => {
+        data = app.null2str(data)
+        if (data.data.code == '1') {
+          data = data.data.data
+          let i = 0
+          for (i in data) {
+            data[i]['createTime'] = app.transformTime(data[i].createTime)
+          }
+          let list = this.data.articleListAll.concat(data)
           this.setData({
-            luntanTxt: res.data
+            articleListAll: list,
+            articleList: list
           })
         } else {
-          wx.showModal({
-            title: '',
-            content: res.msg,
-            showCancel: false
+          // 无数据时
+          wx.showToast({
+            title: data.data.msg,
+            icon: 'none',
+            duration: 2000
           })
         }
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
+  // 文章搜索，根据标题搜索
+  getSearch(e) {
+    wx.request({
+      url: `${app.globalData.requestUrl}/Forum/search`,
+      method: 'POST',
+      data: {
+        title: e.detail.value
+      },
+      success: data => {
+        data = app.null2str(data)
+        if (data.data.code == 1) {
+          this.setData({
+            articleList: data.data.data
+          })
+        } else {
+          this.setData({
+            articleList: this.data.articleListAll
+          })
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
+  // 文章收藏
+  articleCollection(e) {
+    let index = e.currentTarget.dataset.index
+    let articleNode = this.data.articleList[index]
+    let follow = e.currentTarget.dataset.follow
+    wx.request({
+      url: `${app.globalData.requestUrl}/Forum/followPost`,
+      method: 'POST',
+      data: {
+        uid: this.data.userInfo.id,
+        pid: articleNode.id,
+        type: parseInt(follow)
+      },
+      success: data => {
+        data = app.null2str(data)
+        if (data.data.code == 1) {
+          if (follow == 0) {
+            articleNode["is_follow"] = 1
+            articleNode["follow"] = articleNode.follow + 1
+          } else {
+            articleNode["is_follow"] = 0
+            articleNode["follow"] = articleNode.follow - 1
+          }
+          let articleList = this.data.articleList
+          articleList[index] = articleNode
+          this.setData({
+            articleList: articleList
+          })
+        } else {
+          wx.showModal({
+            title: '',
+            content: data.data.msg
+          })
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
+  goDetail(e) {
+    wx.navigateTo({
+      url: `/pages/articleDetail/articleDetail?id=${e.currentTarget.dataset.id}`
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
 
   }
 })
