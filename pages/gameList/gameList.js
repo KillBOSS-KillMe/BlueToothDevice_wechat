f26c08f8832dc1f0900e1f9d504cc5f430c02e48
luow <429175116@qq.com>
2019-08-26 10:41:11 +0800

    游戏列表选中下载项逻辑完成

--- pages/gameList/gameList.js
+++ pages/gameList/gameList.js
@@ -1,37 +1,92 @@
-// pages/quanxian/quanxian.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   gameList:[
-     {
-       name:"狐狸的尾巴",
-       pic1:"/image/luntan1.jpg"
-     },{
-       name:"和平精英",
-       "pic1":"/image/luntan2.jpg"
-     }, {
-       name: "QQ飞车",
-       "pic1": "/image/luntan3.jpg"
-     }, {
-       name: "冰果消消",
-       "pic1": "/image/luntan3.jpg"
-     }, {
-       name: "和平精英",
-       "pic1": "/image/luntan4.jpg"
-     },
-   ]
+    listData:[],
+    selGameList: [],
+    downloadStr: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      userInfo: app.globalData.userInfo,
+      imgUrl: app.globalData.imgUrl
+    })
+    this.getList(options.id)
   },
-
+  getList(id) {
+    wx.request({
+      url: `${app.globalData.requestUrl}/Official/catalog`,
+      method: "POST",
+      data: {
+        type_id: id
+      },
+      success: data => {
+        console.log(data)
+        data = app.null2str(data)
+        if (data.data.code == '1') {
+          data = data.data.data
+          let i = 0
+          for (i in data) {
+            data[i]['str'] = 0
+          }
+          this.setData({
+            listData: data,
+          })
+        } else {
+          wx.showModal({
+            title: '',
+            content: data.data.msg,
+            showCancel: false
+          })
+        }
+      }
+    })
+  },
+  selGame(e) {
+    let index = e.currentTarget.dataset.index
+    let gameStr = e.currentTarget.dataset.index
+    let listData = this.data.listData
+    let selGameList = this.data.selGameList
+    if (gameStr == '1') {
+      listData[index]['str'] = '0'
+      selGameList.splice(index, 1)
+    } else {
+      listData[index]['str'] = '1'
+      selGameList.push(listData[index])
+    }
+    
+    this.setData({
+      listData: listData,
+      selGameList: selGameList
+    })
+  },
+  // 下载按钮点击的事件
+  download() {
+    // 选中的项目大于0项，执行下载
+    if (this.data.selGameList.length > 0) {
+      // 此时进行递归下载
+    } else {
+      // 等于0项，修改按钮状态
+      if (this.data.downloadStr) {
+        this.setData({
+          downloadStr: false,
+          selGameList: []
+        })
+        return false
+      } else {
+        this.setData({
+          downloadStr: true
+        })
+      }
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
